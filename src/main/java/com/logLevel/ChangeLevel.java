69c91bc166d87393b6060e049fa37f8154545033
ruth <ruth.chirinos@mojix.com>
2018-04-15 21:55:55 -0400

    New case to change massively Level of Loggers

--- src/main/java/com/logLevel/ChangeLevel.java
+++ src/main/java/com/logLevel/ChangeLevel.java
@@ -5,6 +5,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChangeLevel {
     public static Logger logger = LogManager.getLogger(ChangeLevel.class);
 
@@ -26,9 +29,23 @@ public class ChangeLevel {
         logger.debug("DEBUG");
         logger.error("ERROR");
         logger.trace("TRACE");
+
+        Map<String, Level> data = new HashMap<>();
+        data.put("com.logLevel",  Level.INFO);
+        changeManyLogLevel(data);
+        System.out.println("--------------");
+        logger.info("INFO");
+        logger.debug("DEBUG");
+        logger.error("ERROR");
+        logger.trace("TRACE");
     }
 
     public static void changeLogLevel(){
         Configurator.setLevel("com.logLevel", Level.ERROR);
+    }
+
+    //https://logging.apache.org/log4j/2.x/log4j-core/apidocs/org/apache/logging/log4j/core/config/Configurator.html#setLevel(java.util.Map)
+    public static void changeManyLogLevel(Map<String, Level> levelMap){
+        Configurator.setLevel(levelMap);
     }
 }
